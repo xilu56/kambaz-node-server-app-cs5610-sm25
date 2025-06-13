41aad0384b622072b77da920725b413ea72c5008
xilu56 <xilu56@gmail.com>
2025-06-13 01:47:48 -0700

    Refactor user data handling in dao.js to directly use db.users for consistency

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,34 +1,32 @@
 import db from "../Database/index.js";
 import { v4 as uuidv4 } from "uuid";
 
-let { users } = db;
-
 export const createUser = (user) => {
   const newUser = { ...user, _id: uuidv4() };
-  users = [...users, newUser];
+  db.users = [...db.users, newUser];
   return newUser;
 };
 
-export const findAllUsers = () => users;
+export const findAllUsers = () => db.users;
 
-export const findUserById = (userId) => users.find((user) => user._id === userId);
+export const findUserById = (userId) => db.users.find((user) => user._id === userId);
 
-export const findUserByUsername = (username) => users.find((user) => user.username === username);
+export const findUserByUsername = (username) => db.users.find((user) => user.username === username);
 
 export const findUserByCredentials = (username, password) =>
-  users.find((user) => user.username === username && user.password === password);
+  db.users.find((user) => user.username === username && user.password === password);
 
 export const updateUser = (userId, updates) => {
-  const userIndex = users.findIndex((user) => user._id === userId);
+  const userIndex = db.users.findIndex((user) => user._id === userId);
   if (userIndex === -1) {
     return null;
   }
   
   // Merge existing user data with updates
-  const updatedUser = { ...users[userIndex], ...updates };
-  users[userIndex] = updatedUser;
+  const updatedUser = { ...db.users[userIndex], ...updates };
+  db.users[userIndex] = updatedUser;
   
   return updatedUser;
 };
 
-export const deleteUser = (userId) => (users = users.filter((u) => u._id !== userId)); 
+export const deleteUser = (userId) => (db.users = db.users.filter((u) => u._id !== userId)); 
