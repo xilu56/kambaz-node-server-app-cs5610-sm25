e5c52a9a5bf25e302f974e06ab8fdcc0e64155f6
xilu56 <xilu56@gmail.com>
2025-06-16 11:39:23 -0700

    Implement user creation and retrieval functions; add filtering by role and name in findAllUsers route

--- Kambaz/Users/dao.js
+++ Kambaz/Users/dao.js
@@ -1,8 +1,98 @@
 import model from "./model.js";
-export const createUser = (user) => {} // implemented later
-export const findAllUsers = () => model.find();
-export const findUserById = (userId) => model.findById(userId);
-export const findUserByUsername = (username) =>  model.findOne({ username: username });
-export const findUserByCredentials = (username, password) =>  model.findOne({ username, password });
-export const updateUser = (userId, user) =>  model.updateOne({ _id: userId }, { $set: user });
-export const deleteUser = (userId) => model.deleteOne({ _id: userId });
+
+export const createUser = async (user) => {
+  try {
+    // Generate a unique ID if not provided
+    if (!user._id) {
+      // Generate a simple numeric ID based on timestamp and random number
+      user._id = Date.now().toString() + Math.floor(Math.random() * 1000).toString();
+    }
+    const newUser = await model.create(user);
+    return newUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findAllUsers = async () => {
+  try {
+    const users = await model.find();
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findUserById = async (userId) => {
+  try {
+    const user = await model.findById(userId);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findUserByUsername = async (username) => {
+  try {
+    const user = await model.findOne({ username: username });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findUserByCredentials = async (username, password) => {
+  try {
+    const user = await model.findOne({ username, password });
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateUser = async (userId, user) => {
+  try {
+    const result = await model.updateOne({ _id: userId }, { $set: user });
+    if (result.modifiedCount > 0) {
+      const updatedUser = await model.findById(userId);
+      return updatedUser;
+    }
+    return null;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteUser = async (userId) => {
+  try {
+    const result = await model.deleteOne({ _id: userId });
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findUsersByRole = async (role) => {
+  try {
+    const users = await model.find({ role: role });
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const findUsersByPartialName = async (partialName) => {
+  try {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    const users = await model.find({
+      $or: [
+        { firstName: { $regex: regex } }, 
+        { lastName: { $regex: regex } },
+        { username: { $regex: regex } }
+      ],
+    });
+    return users;
+  } catch (error) {
+    throw error;
+  }
+};
--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -15,6 +15,17 @@ export default function UserRoutes(app) {
   };
 
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
     const users = await dao.findAllUsers();
     res.json(users);
   };
