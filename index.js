8be06d3f6e0d96bb434eb8a34af14646bbc1aaf1
xilu56 <xilu56@gmail.com>
2025-06-17 16:29:42 -0700

    Set SameSite attribute to "none" for session cookies to support cross-site requests

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(session({
   cookie: {
     secure: isProduction, // true in production with HTTPS
     httpOnly: true,
-    sameSite: isProduction ? "none" : "lax", 
+    sameSite: "none",
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
     domain: isProduction ? undefined : undefined // Let browser decide
   }
