eaf85a120fccee4048acff0cba9fdc36ccf984d5
xilu56 <xilu56@gmail.com>
2025-06-18 17:07:38 -0700

    Update user roles in users.js and schema.js; change TA and ADMIN roles to FACULTY and restrict role options

--- Kambaz/Users/schema.js
+++ Kambaz/Users/schema.js
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema({
     dob: Date,
     role: {
       type: String,
-      enum: ["STUDENT", "FACULTY", "ADMIN", "USER", "TA"],
-      default: "USER",
+      enum: ["STUDENT", "FACULTY"],
+      default: "STUDENT",
     },
     loginId: String,
     section: String,
